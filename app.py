7d9213e8d741f1e48a3beb069cb91e270e7cf843
ericlau00 <ericlau41802@gmail.com>
2019-10-22 08:19:50 -0400

    edit app

--- app.py
+++ app.py
@@ -8,4 +8,27 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+username = "user"
+password = "password"
 
+@app.route("/", methods=["GET"])
+def root():
+    if not 'login' in session:
+        session['login'] = False 
+    if(sessions['login']):
+        return redirect(url_for("home"))
+    else:
+        redirect(url_for("login"))
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template(
+        "home.html"
+    )
+
+@app.route("login", methods=["GET,POST"])
+def login():
+    if(request.method == "POST"):
+        return "this is a post"
+    elif(request.method == "GET"):
+        return "this is a get"
