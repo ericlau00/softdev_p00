311e5b57ecc3ff573ef84871c23aadb317bd3186
ericlau00 <ericlau41802@gmail.com>
2019-10-22 11:45:39 -0400

    clean login logic, fix flashing bug

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-#SeQueL
-#SoftDev1 pd1
-#p00
+# Team SeQueL - Joseph Lee, Yevgeniy Gorbachev, Eric Lau
+# SoftDev1 pd1
+# P0 The Art of Storytellin'
+# 2019-10-28
 
 from flask import Flask, request, redirect, session, render_template, url_for, flash
 import os
@@ -13,29 +14,30 @@ password = "password"
 
 @app.route("/", methods=["GET"])
 def root():
-    if 'user' in session:
+    if not 'login' in session:
+        session['login'] = False
+    if 'user' in session and session['login']:
         return redirect(url_for("home"))
     else:
         return redirect(url_for("login"))
 
 @app.route("/home", methods=["GET"])
 def home():
-    return render_template(
-        "home.html"
-    )
+    if not 'login' in session:
+        session['login'] = False
+    if 'user' in session and session['login']:
+        return render_template("home.html")
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/login", methods=["GET","POST"])
 def login():
     if(request.method == "GET"):
         return render_template("login.html")
     elif(request.method == "POST"):
-        if(request.form['username'] != username):
+        if(request.form['username'] != username or request.form['password'] != password):
+            session['login'] = False
             flash("Failed login!")
-            session['reason'] = 'username'
-            return redirect(url_for("login"))
-        elif(request.form['password'] != password):
-            flash("Failed login!")
-            session['reason'] = "password"
             return redirect(url_for("login"))
         else:
             session['login'] = True
