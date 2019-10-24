797528aadfcd68734db866d186bef33440b8ccc0
ericlau00 <ericlau41802@gmail.com>
2019-10-23 23:53:58 -0400

    plausible working acc.py

--- utl/acc.py
+++ utl/acc.py
@@ -5,8 +5,11 @@ from hashlib import md5
 
 __dbfile__ = '../data/sitedata.db'
 db = sqlite3.connect(__dbfile__)
+c = db.cursor()
 
-#checks account credentials
+c.execute("create table if not exists users (id integer primary_key, username text, password text);")
+
+# #checks account credentials
 def verify_acc(un, pw):
     hpw = md5()
     hpw.update(pw.encode('UTF-8'))
@@ -16,22 +19,22 @@ def verify_acc(un, pw):
     except sqlite3.Error as error:
         print(error)
         return False
-    return [item for item in userinfo][0][1] == hpw
+    return [item for item in userinfo][0][1] == pw
     
-
-
 def push_acc(un, pw):
-    uid = 0
-    with open('counters','w') as cfile:
-        counts = [int(c) for c in '\n'.split(cfile.read())]
-        counts[0] += 1
-        uid = counts[0]
+    exists = c.execute("SELECT EXISTS(SELECT 1 FROM users WHERE username=?);", (un,))
+    if [boolean for boolean in exists][0][0] == 0:
+        uid = count() + 1
         insert(uid, un, pw)
-        cfile.write(str(counts[0]) + '\n' + str(counts[1]))
+        return True
+    return False 
 
 def insert(id, un, pw):
     db.execute('insert into users values (?,?,?);', (id, un, pw))
 
+def count():
+    count = c.execute('select count(*) from users;')
+    return [num for num in count][0][0]
 
 db.commit()
 db.close()
