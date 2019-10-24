97b4468a17d8a4cbc9d58f83680a84144d9f746a
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-24 18:36:21 -0400

    blogs wip

---
+++ utl/blogs.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+__dbfile__ = '../data/sitedata.db'
+
+def create_blog(userid, title):
+    db = sqlite3.connect(__dbfile__)
+    try:
+        db.execute('insert into blogs values (userid, blogid, title) values (?,?,?);', (userid, __count(), title))
+        return True
+    except sqlite3.Error as error:
+        return False
+
+
+def describe(title):
+    db = sqlite3.connect(__dbfile__)
+    try:
+        db.execute('select userid, username, title from blogs where')
+
+
+def __count():
+    db = sqlite3.connect(__dbfile__)
+    count = db.execute('select count(*) from blogs;')
+    return [num for num in count][0][0]
