d860242dae52a37aea99d1238cde5d42078186f8
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-23 21:40:19 -0400

    modified acc

--- utl/acc.py
+++ utl/acc.py
@@ -4,15 +4,14 @@ import sqlite3
 from hashlib import md5
 
 __dbfile__ = '../data/sitedata.db'
-db = sqlite3.connect(__dbfile__)
 
 #checks account credentials
 def verify_acc(un, pw):
-    hpw = md5()
-    hpw.update(pw.encode('UTF-8'))
-    hpw = hpw.hexdigest()
+    hpw = hash(pw)
     try:
-        userinfo = db.execute('select username, password from users where username= ?', (un,))
+        db = sqlite3.connect(__dbfile__)
+        userinfo = db.execute('select username, password from users where username = ?', (un,))
+        db.close()
     except sqlite3.Error as error:
         print(error)
         return False
@@ -20,18 +19,18 @@ def verify_acc(un, pw):
     
 
 
-def push_acc(un, pw):
+def __push_acc(un, pw):
     uid = 0
     with open('counters','w') as cfile:
         counts = [int(c) for c in '\n'.split(cfile.read())]
+        try:
+            db = sqlite3.connect(__dbfile__)
+            db.execute('insert into users values (?,?,?);', (counts[0] + 1, un, pw))
+        except sqlite3.Error as error:
+            return False
         counts[0] += 1
-        uid = counts[0]
-        insert(uid, un, pw)
         cfile.write(str(counts[0]) + '\n' + str(counts[1]))
-
-def insert(id, un, pw):
-    db.execute('insert into users values (?,?,?);', (id, un, pw))
-
-
-db.commit()
-db.close()
+def __hash(txt):
+    hpw = md5()
+    hpw.update(txt.encode('UTF-8'))
+    return hpw.hexdigest()
