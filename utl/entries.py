0a947961c92f296bd2006833e6697f84a3f2c35d
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-25 19:35:26 -0400

    added entry creating and editing

--- utl/entries.py
+++ utl/entries.py
@@ -1,6 +1,5 @@
 import sqlite3
 import os
-import time
 import datetime
 
 
@@ -28,6 +27,23 @@ def init_arc():
 
 def create_entry(userid, blogid, content):
     db = sqlite3.connect(__dbfile__)
+
     count = db.execute('SELECT count(*) FROM entries WHERE blogid=?;',(blogid,))
     count = [item for item in count][0][0]
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
     db.execute('INSERT INTO entries VALUES (?,?,?,?,?)',(blogid, count, 0, time, content))
+    db.execute('INSERT INTO entries_arc VALUES (?,?,?,?,?)',(blogid, count, 0, time, content))
+    db.commit()
+
+def edit_entry(blogid, entryid, content):
+    db = sqlite3.connect(__dbfile__)
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    current = [item for item in db.execute('SELECT versionid FROM entries WHERE blogid=? AND entryid=?',(blogid, entryid))][0][0]
+
+    db.execute('UPDATE entries SET versionid=?, content=?', (current + 1, content))
+    db.execute('INSERT INTO entries_arc VALUES (?,?,?,?,?)', (blogid, entryid, current + 1, time, content))
+    db.commit()
+
+create_entry(1, 0, "This is a test entry for eric's blog")
+edit_entry(0, 0, "This is the edited test entry for eric's blog")
