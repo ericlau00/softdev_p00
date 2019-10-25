de79545ff6c71c762b0d66e3f2aa58059bfe9679
ericlau00 <ericlau41802@gmail.com>
2019-10-24 20:19:19 -0400

    work on db

--- utl/blogs.py
+++ utl/blogs.py
@@ -5,19 +5,27 @@ __dbfile__ = '../data/sitedata.db'
 def create_blog(userid, title):
     db = sqlite3.connect(__dbfile__)
     try:
-        db.execute('insert into blogs values (userid, blogid, title) values (?,?,?);', (userid, __count(), title))
+        db.execute('insert into blogs values (?,?,?);', (userid, count(), title))
+        db.commit()
         return True
     except sqlite3.Error as error:
+        print(error)
         return False
 
 
-def describe(title):
+def describe(id):
     db = sqlite3.connect(__dbfile__)
     try:
-        db.execute('select userid, username, title from blogs where')
+        desc = db.execute('select users.username, users.userid, blogs.title from blogs inner join users on blogs.userid = users.userid where blogs.blogid = ?', (id,))
+        return [data for data in desc][0]
+    except sqlite3.Error as error:
+        print(error)
+        return False
 
-
-def __count():
+def count():
     db = sqlite3.connect(__dbfile__)
     count = db.execute('select count(*) from blogs;')
     return [num for num in count][0][0]
+
+# create_blog(0, "eric's blogs")
+# describe(1)
