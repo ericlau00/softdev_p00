fd090dd4aac49346e6bbe2f61dbf8d8152aab415
ericlau00 <ericlau41802@gmail.com>
2019-10-27 19:13:42 -0400

    clean

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ def delete_entry(blogid,entryid):
 @app.route('/blog/<blogid>/<entryid>/<commentid>/<userid>/delete', methods = ['GET','POST'])
 def delete_comment(blogid,entryid,commentid,userid):
     if 'user' in session:
-        if(session.get('userid') == userid):
+        if(int(session.get('userid')) == int(userid)):
             comments.delete_comment(blogid,entryid,commentid)
             flash('Successfully deleted comment')
             return redirect(url_for('view_entry', blogid = blogid, entryid = entryid))
